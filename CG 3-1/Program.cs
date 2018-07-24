28cf5f878497bd9414821d1852624005e0643504
Kristen Shea <kstar61@live.com>
2018-07-23 22:21:36 -0500

    month numerical to word

--- CG 3-1/Program.cs
+++ CG 3-1/Program.cs
@@ -6,7 +6,45 @@ namespace CG_3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Type the number of a Month (MM): ");
+           
+            string userValue = Console.ReadLine();
+            string message = "";
+
+            if (userValue == "01")
+                message = "January";
+            else if (userValue == "02")
+                message = "February";
+            else if (userValue == "03")
+                message = "March";
+            else if (userValue == "04")
+                message = "April";
+            else if (userValue == "05")
+                message = "May";
+            else if (userValue == "06")
+                message = "June";
+            else if (userValue == "07")
+                message = "July";
+            else if (userValue == "08")
+                message = "August";
+            else if (userValue == "09")
+                message = "September";
+            else if (userValue == "10")
+                message = "October";
+            else if (userValue == "11")
+                message = "November";
+            else if (userValue == "12")
+                message = "December";
+
+            else
+                message = "Invalid Entry";
+        
+            
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+
         }
     }
 }
